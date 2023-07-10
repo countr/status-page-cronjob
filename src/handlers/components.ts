64d6f7333c7410562c24efeeaa88e08e8482657f
Promise <me@promise.solutions>
2023-07-10 08:22:27 +0000

    fix: set threshold for "high ping" a little higher
    Signed-off-by: Promise <me@promise.solutions>

--- src/handlers/components.ts
+++ src/handlers/components.ts
@@ -50,7 +50,7 @@ function getUpdates(shard: CountrApiShardData | null): [InstatusComponentStatus,
   const ping = Math.ceil(shard?.ping ?? 0);
 
   if (status === Status.Ready) {
-    if (ping < 125) return ["OPERATIONAL"];
+    if (ping < 150) return ["OPERATIONAL"];
     return ["DEGRADEDPERFORMANCE", `High ping of ${ping}ms`];
   }
   if (status === Status.Connecting) return ["PARTIALOUTAGE", "Connecting"];
